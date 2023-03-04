6492496343f814538f990c7aa8485e354ea5ae61
maxxxondagon <maxxondagon@gmail.com>
2023-03-04 12:07:48 +0500

    Initial Commit

---
+++ 2nd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2nd
+//
+//  Created by Maxim Soloboev on 04.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
